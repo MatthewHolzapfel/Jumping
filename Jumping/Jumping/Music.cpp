c4acedd871b13b14b2bdfe2098a51f693adc59f8
Maddie273 <madalynkendrick@gmail.com>
2019-01-31 10:56:38 -0500

    Adding new .cpp and .h files for some tasks
    Temporarily commented out to avoid causing errors and until SFML is implemented into new visual studio project

---
+++ Jumping/Jumping/Music.cpp
@@ -0,0 +1,17 @@
+/*
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+
+#include <SFML/Audio.hpp>
+
+using namespace std;
+
+
+int main() {
+	sf::Music music;
+	//if (!music.openFromFile("MarioBGM.ogg"))
+	music.openFromFile("Jump-Over-Robots-master/JumpOverRobots/MarioBGM.ogg");
+	//return EXIT_FAILURE;
+	music.setVolume(50);
+	music.play(); */
