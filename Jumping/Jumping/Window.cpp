0ba31588e4ede4ec4270b09d49060f511919e16b
ken-kun <kevin.ly96@live.ca>
2019-02-03 21:34:31 -0500

    Add files via upload

--- Jumping/Jumping/Window.cpp
+++ Jumping/Jumping/Window.cpp
@@ -1,13 +1,29 @@
 #include "Window.h"
 
 Window::Window()
-{}
+{
+}
 
 void Window::Initialize(int _width, int _height)
 {
-	window = new sf::RenderWindow (sf::VideoMode(_width, _height), "Jump!");
+	sf::RenderWindow window(sf::VideoMode(_width, _height), "Jump!");
 	sf::CircleShape shape(100.f);
 	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
 
 }
 
