3ad9c3f7aef551747442aefd0e698f3f248916b5
ken-kun <kevin.ly96@live.ca>
2019-02-07 10:16:40 -0500

    Add files via upload

--- Jumping/Jumping/Music.cpp
+++ Jumping/Jumping/Music.cpp
@@ -1,24 +1,24 @@
 
-#include "Music.h"
-#include <iostream>
-
-using namespace std;
-
-Music::Music()
-{
-}
-
-void Music::PlayMusic() {
-	sf::Music music;
-	if (!music.openFromFile("MarioBGM.ogg")) {
-		cout << "Music Error";
-	}
-	music.setVolume(50);
-	music.setLoop(true);
-	music.play();
-}
-
-Music::~Music()
-{
-}
-
+//#include "Music.h"
+//#include <iostream>
+//
+//using namespace std;
+//
+//Music::Music()
+//{
+//}
+//
+//void Music::PlayMusic() {
+//	sf::Music music;
+//	if (!music.openFromFile("MarioBGM.ogg")) {
+//		cout << "Music Error";
+//	}
+//	music.setVolume(50);
+//	music.setLoop(true);
+//	music.play();
+//}
+//
+//Music::~Music()
+//{
+//}
+//
--- Jumping/Jumping/main.cpp
+++ Jumping/Jumping/main.cpp
@@ -1,11 +1,31 @@
 #include "Jumping.h"
 #include <iostream>
+#include "SFML/Graphics.hpp"
+#include "Window.h"
 
 
 int main()
 {
 	if (Jumping::Instance()->Start())
 		Jumping::Instance()->Open();
+	sf::RenderWindow window(sf::VideoMode(600, 600), "SFML");
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		//window.draw(shape);
+		window.display();
+	}
+
+
+
 	int a;
 	std::cin >> a;
 	return 0;
