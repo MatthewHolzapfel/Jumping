f7b85bec5425f5e8f781edfa0a0a3d7c649d78fb
Maddie273 <madalynkendrick@gmail.com>
2019-03-01 20:38:44 -0500

    commented attempted changes to code

--- Jumping/Jumping/Timer.h
+++ Jumping/Jumping/Timer.h
@@ -19,63 +19,123 @@ public:
 		sf::Sprite sprite;
 		sprite.setTexture(test);
 
+		
+				sf::Time micro = sf::microseconds(1000);
+				sf::Time milli = sf::milliseconds(10);
+				sf::Time second = sf::seconds(0.01f);
+				std::cout << "Seconds per milliseconds: " << micro.asSeconds() << std::endl;
+				std::cout << "Milliseconds per millisecond: " << micro.asMilliseconds() << std::endl;
+				std::cout << "Microseconds per millisecond: " << micro.asMicroseconds() << std::endl;
+				std::cout << (micro + micro).asSeconds() << std::endl;
 
-		sf::Time micro = sf::microseconds(1000);
-		sf::Time milli = sf::milliseconds(10);
-		sf::Time second = sf::seconds(0.01f);
-		std::cout << "Seconds per milliseconds: " << micro.asSeconds() << std::endl;
-		std::cout << "Milliseconds per millisecond: " << micro.asMilliseconds() << std::endl;
-		std::cout << "Microseconds per millisecond: " << micro.asMicroseconds() << std::endl;
-		std::cout << (micro + micro).asSeconds() << std::endl;
+				sf::Clock clock;
 
-		sf::Clock clock;
+			
 
+				sf::Time elapsed = clock.getElapsedTime();
+			
 
-		while (window.isOpen())
-		{
-			sf::Event event;
-			window.setKeyRepeatEnabled(false);
-			while (window.pollEvent(event))
-			{
-				switch (event.type)
+				while (window.isOpen())
 				{
-					case sf::Event::KeyPressed:
-						if (event.key.code == sf::Keyboard::Space)
+					
+					sf::Event event;
+					window.setKeyRepeatEnabled(false);
+					while (window.pollEvent(event))
+					{
+						switch (event.type)
 						{
-							std::cout << "Jump Player" << std::endl;
+						case sf::Event::KeyPressed:
+							if (event.key.code == sf::Keyboard::Space) 
+							{
+								std::cout << "Jump Player" << std::endl;
+								
+							}
+							break;
+
+						case sf::Event::Closed:
+							window.close();
+							return;
+
+						case sf::Event::KeyReleased:
+							clock.restart();
+							break;
+
 						}
-						break;
+						//if (elapsed.asSeconds() < 20.0f)
+						//	window.close();
+						
+						//return;
+					}
 
-				case sf::Event::Closed:
-					window.close();
-					return;
+					//update the game
+					
+					//if (elapsed.asSeconds() < 6.0f)
+					//	std::cout << elapsed.asSeconds() << std::endl;
+					sf::Time elapsed = clock.getElapsedTime();
+					if (elapsed.asSeconds() < 1.0f)
+						std::cout << elapsed.asSeconds() << std::endl;
 
-				case sf::Event::KeyReleased:
-					clock.restart();
-					break;
+
+					window.clear();
+					window.draw(shape);
+					window.draw(sprite);
+
+					window.display();
+
+					//if (elapsed.asSeconds() < 5.0f)
+						//window.close();
+					//return; 
+
+
+
+
+					
+
+	}
+};
+
+	
+	void Update() 
+	{
+		sf::Clock clock;
+		sf::Event event;
+		
+		//while (event.pollEvent)
+		{
+			switch (event.type)
+			{
+			case sf::Event::KeyPressed:
+				if (event.key.code == sf::Keyboard::Space)
+				{
+					std::cout << "Jump Player" << std::endl;
+
 				}
+				break;
+
+			case sf::Event::Closed:
+				//window.close();
+				return;
+
+			case sf::Event::KeyReleased:
+				clock.restart();
+				break;
+
 			}
-
-			//update the game
-			sf::Time elapsed = clock.getElapsedTime();
-			if (elapsed.asSeconds() < 1.0f)
-				std::cout << elapsed.asSeconds() << std::endl;
-
-			window.clear();
-			window.draw(shape);
-			window.draw(sprite);
-
-			window.display();
-
-
-
-
-
+			
 		}
-	};
+
+		
+		//sf::Clock clock;
+		//update the game
+		sf::Time elapsed = clock.getElapsedTime();
+		if (elapsed.asSeconds() < 1.0f)
+			std::cout << elapsed.asSeconds() << std::endl; 
+	}
+
 
 	~Timer()
 	{
 	};
 
-};
+}; 
+	
