ad3609c13b3c55135f4b6533f54ea0e1914b80c5
ken-kun <kevin.ly96@live.ca>
2019-03-02 14:08:43 -0500

    Added background

--- Jumping/Jumping/main.cpp
+++ Jumping/Jumping/main.cpp
@@ -7,6 +7,7 @@
 #include <SFML/Audio.hpp>
 #include "SoundEffects.h"
 
+
 using namespace std;
 
 int main()
@@ -14,19 +15,27 @@ int main()
 	if (Jumping::Instance()->Start())
 		Jumping::Instance()->Open();
 
-	sf::RenderWindow window(sf::VideoMode(600, 600), "SFML");
+	sf::RenderWindow window(sf::VideoMode(1000, 600), "SFML");
 	sf::RectangleShape *shape1 = new sf::RectangleShape;
 	shape1->setSize(sf::Vector2f(200, 100));
 	shape1->setFillColor(sf::Color::Green);
 	shape1->setPosition(sf::Vector2f(window.getSize().x / 2, window.getSize().y / 2));
 	shape1->setOrigin(sf::Vector2f(shape1->getSize().x / 2, shape1->getSize().y / 2));
 	
+	sf::Texture background;
+	background.loadFromFile("Background.png");
+	sf::Sprite sprite(background);
+	sprite.scale(sf::Vector2f(0.5f, 0.5f));
+
+	
+	
+
 
 	Music myMusic;
 	myMusic.PlayMusic("MarioBGM.ogg");
 	
 
-
+	;
 
 	while (window.isOpen())
 	{
@@ -45,7 +54,10 @@ int main()
 
 
 		window.clear();
+		
+		window.draw(sprite);
 		window.draw(*shape1);
+		
 		window.display();
 		
 	}
